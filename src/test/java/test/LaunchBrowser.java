5ab0febdfb06e32cd21691b60c7cbfa0a4d022e6
dungltk <lethikimdung372@gmail.com>
2022-04-23 17:02:56 +0700

    update lesson 15

--- src/test/java/test/LaunchBrowser.java
+++ src/test/java/test/LaunchBrowser.java
@@ -1,4 +1,23 @@
 package test;
 
-public class LauchBrowser {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LaunchBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        String currentProjectLocation = System.getProperty("user.dir");
+        String relativeChromeDriverLocation = "\\src\\test\\resources\\drivers\\chromedriver.exe";
+        String chromeDriverPath = currentProjectLocation + relativeChromeDriverLocation;
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://github.com/dungltk96");
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
 }
+
